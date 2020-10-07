5892daccfb3cc8c188aa3e08af9cd10bba89c6da
yuya <f-oinuma@hotmail.co.jp>
2020-10-07 07:38:04 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,11 @@ class ApplicationController < ActionController::Base
       redirect_to login_url
     end
   end
+  
+  def counts(user)
+    @count_microposts = user.microposts.count
+  end
+  
 end
   
   
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+  
+  resources :microposts, only: [:create, :destroy]
+
     
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
