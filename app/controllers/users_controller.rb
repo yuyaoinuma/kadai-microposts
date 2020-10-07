6fb41e32191430534b18df2e50158f34e0d6e6fa
yuya <f-oinuma@hotmail.co.jp>
2020-10-07 04:07:55 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+   before_action :require_user_logged_in, only: [:index, :show]
+  
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -22,10 +24,16 @@ class UsersController < ApplicationController
       render :new
     end
   end
+    
+    
+
+
   
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+  
+
 end
